6ef95a52eedd7f5afbaeae7441efd266e85799f9
VZyma <112830236+VZyma@users.noreply.github.com>
2024-10-02 00:09:48 +0300

    hw update 2.0

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,15 +1,6 @@
-import createHttpError from 'http-errors';
-
-const errorHandler = (error, req, res, next) => {
-  if (!error.status) {
-    error = createHttpError(500, 'Something went wrong');
-  }
-
-  res.status(error.status || 500).json({
-    status: error.status || 500,
-    message: error.message || 'Something went wrong',
-    data: error.expose ? error.message : 'Internal Server Error',
+export const errorHandler = (err, req, res, next) => {
+  const { status = 500, message } = err;
+  res.status(status).json({
+    message,
   });
 };
-
-export default errorHandler;
