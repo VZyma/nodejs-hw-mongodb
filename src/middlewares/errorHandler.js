34c506644abd4d43ebb41a240588bd2421b59ecc
VZyma <112830236+VZyma@users.noreply.github.com>
2024-10-04 17:09:57 +0300

    hw 3.0

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,14 +1,26 @@
 import createHttpError from 'http-errors';
 
 const errorHandler = (error, req, res, next) => {
-  if (!error.status) {
-    error = createHttpError(500, 'Something went wrong');
+  let status = error.status || 500;
+  let message = error.message || 'Something went wrong';
+  let data = error.expose ? error.message : 'Internal Server Error';
+
+  // Обработка ошибок валидации Mongoose
+  if (error.name === 'ValidationError') {
+    status = 400; // Bad Request
+    message = 'Validation Error';
+    data = {};
+
+    // Собираем все ошибки валидации в один объект
+    for (let field in error.errors) {
+      data[field] = error.errors[field].message;
+    }
   }
 
-  res.status(error.status || 500).json({
-    status: error.status || 500,
-    message: error.message || 'Something went wrong',
-    data: error.expose ? error.message : 'Internal Server Error',
+  res.status(status).json({
+    status,
+    message,
+    data,
   });
 };
 
