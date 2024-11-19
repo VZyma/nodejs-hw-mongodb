dd98c7f44f03969c6882e3d8216ee76b66a2339f
VZyma <112830236+VZyma@users.noreply.github.com>
2024-11-19 03:49:23 +0200

    s

--- src/server.js
+++ src/server.js
@@ -1,98 +1,46 @@
-// import express from "express";
-// import cors from "cors";
-// import pino from 'pino-http';
-// import dotenv from 'dotenv';
-// import { env } from "./utils/env.js";
-// import router from './routers/index.js';
-// import { errorHandler } from "./middlewares/errorHandler.js";
-// import { notFoundHandler } from "./middlewares/notFoundHandler.js";
-// import cookieParser from 'cookie-parser';
-
-// import { UPLOAD_DIR } from "./constants/index.js";
-
-// import { swaggerDocs } from "./middlewares/swaggerDocs.js";
-
-// dotenv.config();
-// const PORT = Number(env("PORT", 3000));
-
-
-// export const setupServer = () => {
-//     const app = express();
-
-//     app.use(express.json());
-//     app.use(cors());
-//     app.use(cookieParser());
-
-//     app.use(
-//         pino({
-//           transport: {
-//             target: 'pino-pretty',
-//           },
-//         }),
-//     );
-
-//     app.use('/uploads', express.static(UPLOAD_DIR));
-//     app.use('/api-docs', swaggerDocs());
-
-//     app.use(router);
-
-//     app.use(notFoundHandler);
-//     app.use(errorHandler);
-
-
-//     app.listen(PORT, () => {
-//         console.log(`This server is running on PORT ${PORT}`);
-//     });
-// };
-import path from 'node:path';
-
-import express from 'express';
-import cookieParser from 'cookie-parser';
-import cors from 'cors';
+import express from "express";
+import cors from "cors";
 import pino from 'pino-http';
-import 'dotenv/config';
-import { initMongoConnection } from './db/initMongoConnection.js';
+import dotenv from 'dotenv';
+import { env } from "./utils/env.js";
+import router from './routers/index.js';
+import { errorHandler } from "./middlewares/errorHandler.js";
+import { notFoundHandler } from "./middlewares/notFoundHandler.js";
+import cookieParser from 'cookie-parser';
 
-import routers from './routers/index.js';
-import { notFoundHandler } from './middlewares/notFoundHandler.js';
-import { errorHandler } from './middlewares/errorHandler.js';
+import { UPLOAD_DIR } from "./constants/index.js";
 
-import { swaggerDocs } from './middlewares/swaggerDocs.js';
+import { swaggerDocs } from "./middlewares/swaggerDocs.js";
 
-const app = express();
+dotenv.config();
+const PORT = Number(env("PORT", 3000));
 
-app.use('/api-docs', swaggerDocs());
 
-app.use('/photos', express.static(path.resolve('src', 'public/photos')));
+export const setupServer = () => {
+    const app = express();
 
-export const setupServer = async () => {
-  try {
-    await initMongoConnection();
+    app.use(express.json());
+    app.use(cors());
+    app.use(cookieParser());
 
-    const PORT = process.env.PORT || 3000;
+    app.use(
+        pino({
+          transport: {
+            target: 'pino-pretty',
+          },
+        }),
+    );
+
+    app.use('/uploads', express.static(UPLOAD_DIR));
+    app.use('/api-docs', swaggerDocs());
+
+    app.use(router);
+
+    app.use(notFoundHandler);
+    app.use(errorHandler);
+
 
     app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`);
+        console.log(`This server is running on PORT ${PORT}`);
     });
-  } catch (error) {
-    console.error(error);
-  }
-
-  app.use(cors());
-  app.use(cookieParser());
-  app.use(
-    pino({
-      transport: {
-        target: 'pino-pretty',
-      },
-    }),
-  );
-
-  app.use('/', routers);
-
-  app.use(notFoundHandler);
-  app.use(errorHandler);
 };
-
-export default app;
-
