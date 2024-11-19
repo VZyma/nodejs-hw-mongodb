c4b0d1a22f5a32988ede4e37152c9536ead4c461
VZyma <112830236+VZyma@users.noreply.github.com>
2024-11-19 02:05:25 +0200

    Up server

--- src/server.js
+++ src/server.js
@@ -1,3 +1,50 @@
+// import express from "express";
+// import cors from "cors";
+// import pino from 'pino-http';
+// import dotenv from 'dotenv';
+// import { env } from "./utils/env.js";
+// import router from './routers/index.js';
+// import { errorHandler } from "./middlewares/errorHandler.js";
+// import { notFoundHandler } from "./middlewares/notFoundHandler.js";
+// import cookieParser from 'cookie-parser';
+
+// import { UPLOAD_DIR } from "./constants/index.js";
+
+// import { swaggerDocs } from "./middlewares/swaggerDocs.js";
+
+// dotenv.config();
+// const PORT = Number(env("PORT", 3000));
+
+
+// export const setupServer = () => {
+//     const app = express();
+
+//     app.use(express.json());
+//     app.use(cors());
+//     app.use(cookieParser());
+
+//     app.use(
+//         pino({
+//           transport: {
+//             target: 'pino-pretty',
+//           },
+//         }),
+//     );
+
+//     app.use('/uploads', express.static(UPLOAD_DIR));
+//     app.use('/api-docs', swaggerDocs());
+
+//     app.use(router);
+
+//     app.use(notFoundHandler);
+//     app.use(errorHandler);
+
+
+//     app.listen(PORT, () => {
+//         console.log(`This server is running on PORT ${PORT}`);
+//     });
+// };
+
 import express from "express";
 import cors from "cors";
 import pino from 'pino-http';
@@ -15,12 +62,19 @@ import { swaggerDocs } from "./middlewares/swaggerDocs.js";
 dotenv.config();
 const PORT = Number(env("PORT", 3000));
 
-
 export const setupServer = () => {
     const app = express();
 
     app.use(express.json());
-    app.use(cors());
+
+    // Настройка CORS
+    app.use(cors({
+        origin: '*', // Разрешить запросы с любого домена
+        methods: ['GET', 'POST', 'PUT', 'DELETE'], // Разрешенные методы
+        allowedHeaders: ['Content-Type', 'Authorization'], // Разрешенные заголовки
+        credentials: true // Разрешить отправку куки и заголовков авторизации
+    }));
+
     app.use(cookieParser());
 
     app.use(
@@ -39,9 +93,7 @@ export const setupServer = () => {
     app.use(notFoundHandler);
     app.use(errorHandler);
 
-
     app.listen(PORT, () => {
         console.log(`This server is running on PORT ${PORT}`);
     });
 };
-
