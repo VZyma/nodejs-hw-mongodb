55403ed6d55c7c410e14dae654b20f708e3da441
VZyma <112830236+VZyma@users.noreply.github.com>
2024-11-20 11:11:53 +0200

    up

--- src/server.js
+++ src/server.js
@@ -1,46 +1,47 @@
-import express from "express";
-import cors from "cors";
-import pino from 'pino-http';
-import dotenv from 'dotenv';
-import { env } from "./utils/env.js";
-import router from './routers/index.js';
-import { errorHandler } from "./middlewares/errorHandler.js";
-import { notFoundHandler } from "./middlewares/notFoundHandler.js";
+import path from 'node:path';
+import express from 'express';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import pino from 'pino-http';
+import 'dotenv/config';
+import { initMongoConnection } from './db/initMongoConnection.js';
+import routers from './routers/index.js';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
+import { errorHandler } from './middlewares/errorHandler.js';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
-import { UPLOAD_DIR } from "./constants/index.js";
+const app = express();
 
-import { swaggerDocs } from "./middlewares/swaggerDocs.js";
+app.use('/api-docs', swaggerDocs());
+app.use('/photos', express.static(path.resolve('src', 'public/photos')));
 
-dotenv.config();
-const PORT = Number(env("PORT", 3000));
+export const setupServer = async () => {
+  try {
+    await initMongoConnection();
 
-
-export const setupServer = () => {
-    const app = express();
-
-    app.use(express.json());
-    app.use(cors());
-    app.use(cookieParser());
-
-    app.use(
-        pino({
-          transport: {
-            target: 'pino-pretty',
-          },
-        }),
-    );
-
-    app.use('/uploads', express.static(UPLOAD_DIR));
-    app.use('/api-docs', swaggerDocs());
-
-    app.use(router);
-
-    app.use(notFoundHandler);
-    app.use(errorHandler);
-
+    const PORT = process.env.PORT || 3000;
 
     app.listen(PORT, () => {
-        console.log(`This server is running on PORT ${PORT}`);
+      console.log(`Server is running on port ${PORT}`);
     });
+  } catch (error) {
+    console.error(error);
+  }
+
+  app.use(cors());
+  app.use(cookieParser());
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
+
+  app.use('/', routers);
+
+  app.use(notFoundHandler);
+  app.use(errorHandler);
 };
+
+export default app;
