13817a8789d693eeb546ff7e953b0d6e63b2f849
VZyma <112830236+VZyma@users.noreply.github.com>
2024-11-21 23:42:14 +0200

    Update

--- src/server.js
+++ src/server.js
@@ -1,47 +1,42 @@
-import path from 'node:path';
-import express from 'express';
-import cookieParser from 'cookie-parser';
-import cors from 'cors';
+import express from "express";
+import cors from "cors";
 import pino from 'pino-http';
-import 'dotenv/config';
-import { initMongoConnection } from './db/initMongoConnection.js';
-import routers from './routers/index.js';
-import { notFoundHandler } from './middlewares/notFoundHandler.js';
-import { errorHandler } from './middlewares/errorHandler.js';
-import { swaggerDocs } from './middlewares/swaggerDocs.js';
+import dotenv from 'dotenv';
+import { env } from "./utils/env.js";
+import router from './routers/index.js';
+import { errorHandler } from "./middlewares/errorHandler.js";
+import { notFoundHandler } from "./middlewares/notFoundHandler.js";
+import cookieParser from 'cookie-parser';
+import { UPLOAD_DIR } from "./constants/index.js";
+import { swaggerDocs } from "./middlewares/swaggerDocs.js";
 
-const app = express();
+dotenv.config();
+const PORT = Number(env('PORT', '3000'));
 
-app.use('/api-docs', swaggerDocs());
-app.use('/photos', express.static(path.resolve('src', 'public/photos')));
 
-export const setupServer = async () => {
-  try {
-    await initMongoConnection();
+export const setupServer = () => {
+    const app = express();
+    app.use(express.json());
+    app.use(cors());
+    app.use(cookieParser());
+    app.use(
+        pino({
+          transport: {
+            target: 'pino-pretty',
+          },
+        }),
+    );
 
-    const PORT = process.env.PORT || 3000;
+  app.use('/api-docs', swaggerDocs());
 
-    app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`);
-    });
-  } catch (error) {
-    console.error(error);
-  }
+  app.use(router);
 
-  app.use(cors());
-  app.use(cookieParser());
-  app.use(
-    pino({
-      transport: {
-        target: 'pino-pretty',
-      },
-    }),
-  );
-
-  app.use('/', routers);
+  app.use('/uploads', express.static(UPLOAD_DIR));
 
   app.use(notFoundHandler);
   app.use(errorHandler);
-};
 
-export default app;
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+};
